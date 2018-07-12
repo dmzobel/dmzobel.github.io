5eed38032b8f00eccc648010008519cd8e8e41ae
Marshall Zobel <dmzobel@gmail.com>
2018-07-12 18:21:28 -0400

    add LinkedIn logo to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <div className="navbar">
-    <Link to="/">
-      <h1>Marshall Zobel</h1>
-    </Link>
-    <Link to="/timeline">Timeline</Link>
-    <Link to="/projects">Projects</Link>
+    <div>
+      <Link to="/">
+        <h1>Marshall Zobel</h1>
+      </Link>
+      <Link to="/timeline">Timeline</Link>
+      <Link to="/projects">Projects</Link>
+    </div>
+    <div>
+      <Link to="https://www.linkedin.com/in/marshallzobel/">
+        <img
+          src="http://1000logos.net/wp-content/uploads/2017/03/LinkedIn-Logo-500x500.png"
+          alt=""
+          className="icon"
+        />
+      </Link>
+    </div>
   </div>
 );
 
