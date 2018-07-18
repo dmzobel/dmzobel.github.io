95a0b11f601b3ba448cc0ed959c803f025f60f03
Marshall Zobel <dmzobel@gmail.com>
2018-07-18 10:50:46 -0400

    completed Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { bhDemo1, bhDemo2 } from '../images';
+import { bhDemo1, bhDemo2, vintage, wizBang } from '../images';
 
 const Projects = () => (
   <React.Fragment>
@@ -21,38 +21,46 @@ const Projects = () => (
       <img src={bhDemo2} alt="" className="screenshot" />
     </div>
     <div className="project-section">
-      <img src={bhDemo1} alt="" className="screenshot" />
       <div>
-        <h1>Banana Hunt</h1>
+        <h1>Vintage</h1>
         <br />
         <p>
-          Banana Hunt is a mobile game for iOS that takes users on an augmented
-          reality (AR) scavenger hunt. The goal is collect all of the virtual
-          bananas as fast as possible, and earn a place on the leaderboard!
-          Users can also create their own custom scavenger hunts, placing
-          virtual bananas in their surrounding environment. You can drop as many
-          bananas as you want, wherever you want, in as wide an area as you
-          want, and then see who can capture them the quickest!
+          To the everyday consumer, wine can be an intimidating purchase.
+          Vintage hopes to mitigate some of those feelings by narrowing the
+          consumer's focus to a select few regions and vintages that have a high
+          likelihood of producing a good quality wine.
+        </p>
+        <br />
+        <p>
+          Vintage is a data modeling web application that allows users to
+          compare the quality of different wine vintages. Using the{' '}
+          <inline>
+            <a href="https://www.ncdc.noaa.gov/cdo-web/webservices/v2">
+              NOAA Climate Data API
+            </a>
+          </inline>
+          , the application gathers climate data from various regions around the
+          world and feeds the data into Princeton economist Orley Ashenfelter's
+          Bordeaux Equation, generating a score for each vintage that is
+          predictive of the wine's quality.
         </p>
       </div>
-      <img src={bhDemo2} alt="" className="screenshot" />
+      <img src={vintage} alt="" className="screenshot-desktop" />
     </div>
     <div className="project-section">
-      <img src={bhDemo1} alt="" className="screenshot" />
+      <img src={wizBang} alt="" className="screenshot-desktop" />
       <div>
-        <h1>Banana Hunt</h1>
+        <h1>WizBang</h1>
         <br />
         <p>
-          Banana Hunt is a mobile game for iOS that takes users on an augmented
-          reality (AR) scavenger hunt. The goal is collect all of the virtual
-          bananas as fast as possible, and earn a place on the leaderboard!
-          Users can also create their own custom scavenger hunts, placing
-          virtual bananas in their surrounding environment. You can drop as many
-          bananas as you want, wherever you want, in as wide an area as you
-          want, and then see who can capture them the quickest!
+          WizBang is an E-commerce web application offering magical products to
+          wannabe wizards and witches. Users can browse a list of products and
+          artifacts, add the desired items to their cart, and checkout using
+          Stripe. The application also incorporates OAuth functionality,
+          allowing users to save items to a Wish List and view their previous
+          orders.
         </p>
       </div>
-      <img src={bhDemo2} alt="" className="screenshot" />
     </div>
   </React.Fragment>
 );
--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,5 @@
 export { default as Marshall } from './Marshall.jpg';
 export { default as bhDemo1 } from './bh-demo1.png';
 export { default as bhDemo2 } from './bh-demo2.png';
+export { default as vintage } from './vintage.png';
+export { default as wizBang } from './wizBang.png';
